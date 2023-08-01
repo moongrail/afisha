f285891a4d79c95dd387091482fa8702319564e9
Moongrail <omgone666@yandex.ru>
2023-08-01 10:44:16 +0300

    fix: import.

--- stats/stats-server/src/main/java/ru/practicum/stat_server/repositories/StatisticRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/stat_server/repositories/StatisticRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.stat_server.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.stat_server.model.Statistic;
 
