0c6c33e46fcb94313473a0f20a227855b0719938
Moongrail <omgone666@yandex.ru>
2023-08-09 12:49:35 +0300

    fix: validation and logic.

--- main/src/main/java/ru/practicum/main/request/repositories/RequestRepository.java
+++ main/src/main/java/ru/practicum/main/request/repositories/RequestRepository.java
@@ -1,12 +1,8 @@
 package ru.practicum.main.request.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.main.request.model.Request;
-import ru.practicum.main.request.model.RequestStatus;
 
 import java.util.List;
 
